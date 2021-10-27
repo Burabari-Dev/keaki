7886e4e21f9cbb9b8c8582ab59c3755190b73148
Burabari <burabaridev@gmail.com>
2021-10-27 05:21:24 +0100

    add FirebaseProvider to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { ProvideFirebase } from '../lib/firebase/FirebaseProvider';
+// import { ProvideAuth } from "../lib/firebase/AuthProvider";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProvideFirebase>
+      {/* <ProvideAuth> */}
+        <Component {...pageProps} />
+      {/* </ProvideAuth> */}
+    </ProvideFirebase>
+  )
 }
 
 export default MyApp
