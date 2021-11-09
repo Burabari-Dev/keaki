8ce3f789995d240cd96bfaba74775ddc7f4be5c0
Burabari <burabaridev@gmail.com>
2021-11-09 18:53:28 +0100

    add primereact css files to global _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import { ProvideFirebase } from '../lib/firebase/FirebaseProvider';
 // import { ProvideAuth } from "../lib/firebase/AuthProvider";
-import '../styles/globals.css'
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
