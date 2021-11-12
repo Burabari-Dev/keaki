94bccb278e688b8ab95ee6e56cb7ea2b568ed400
Burabari <burabaridev@gmail.com>
2021-11-12 06:21:40 +0100

    re-structure home page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-// import Image from 'next/image'
-import Container from '../components/ui/Container';
-import Header from '../components/ui/Header';
-import Footer from '../components/ui/Footer';
-import styles from '../styles/Home.module.css';
-import TopBar from '../components/ui/TopBar/TopBar';
+import Layout from '../components/ui/Layout';
+import HomePage from '../components/pages/HomePage';
+import { Menu_Items as data } from "../../dev_data/data"; //TODO: Remove this later
 
-export default function Home() {
-
+const Home = () => {
+  //TODO: App Data Provider initialize here
   return (
     <>
-      <Container>
-        <Header />
-        <main className={styles.main}>
-          <TopBar/>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://keaki.com">Homegage!</a>
-          </h1>
-
-        </main>
-      </Container>
-      <Footer />
+      <Layout pageTitle={null} menuData={data} >
+        <HomePage/>
+      </Layout>
     </>
   )
 }
+
+export default Home;
