b22cdd3f364773555ad8151f76abbd8c71d441a1
Burabari <burabaridev@gmail.com>
2021-11-12 05:46:45 +0100

    create Banner component

---
+++ components/ui/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../styles/Banner.module.css';
+
+const Banner = ({data}) => {
+  return(
+    <div className={styles.Banner}> Banner Area </div>
+  )
+}
+
+export default Banner;
