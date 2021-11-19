8a180561bb1344b0201b6a5dc01094b4e3a2a1b6
Burabari <burabaridev@gmail.com>
2021-11-19 04:39:02 +0100

    reformat homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/ui/Layout';
 import HomePage from '../components/pages/HomePage';
-import { Menu_Items as data } from "../../dev_data/data"; //TODO: Remove this later
 
 const Home = () => {
   //TODO: App Data Provider initialize here
+  const [menuItems, setMenuItems] = useState();
+
+  useEffect(async () => {
+    const isDev = process.env['NODE_ENV'].toLowerCase() === 'development';
+    if (isDev) {
+      let { menu_Items } = await import('../../dev_data/data.js');
+      setMenuItems(menu_Items);
+    } else {
+      //TODO: Load menuData from server
+    }
+  }, [menuItems]);
+
   return (
     <>
-      <Layout pageTitle={null} menuData={data} >
-        <HomePage/>
+      <Layout pageTitle={null} menuData={menuItems} >
+        <HomePage />
       </Layout>
     </>
   )
