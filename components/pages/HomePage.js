46cd061dd3c2529653dac4314e8ca95ec54e93dd
Burabari <burabaridev@gmail.com>
2021-11-20 13:56:04 +0100

    add PromoReel to HomePage

--- components/pages/HomePage.js
+++ components/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from '../ui/Banner';
 import styles from '../../styles/HomePage.module.css';
+import PromoReel from '../ui/PromoReel';
 
 const HomePage = () => {
   return(
     <div className={styles.HomePage}>
       <Banner />
+      <PromoReel title='Electronics' />
+      <PromoReel title='Computers' />
+      <PromoReel title='Mobile Phones' />
     </div>
   )
 }
