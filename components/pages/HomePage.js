8eec80ae3b043032bd66ccaec5133dd7d87ef1ce
Burabari <burabaridev@gmail.com>
2021-11-25 12:06:20 +0100

    moved dev data for ProductReel to homepage

--- components/pages/HomePage.js
+++ components/pages/HomePage.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Banner from '../ui/Banner';
 import styles from '../../styles/HomePage.module.css';
 import PromoReel from '../ui/PromoReel';
 
 const HomePage = () => {
-  /**DATA POINTS
-   * 
-   * {
-   *  Electronics: [{pix: 'url', price: '3.99', dprice: '3.50', rating: ''}],
-   *  Computers: {},
-   *  Mobile Phones: {},
-   * }
-   */
-  return(
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getDevData() {
+      const { Prod_Reel_Data } = await import('../../dev/data');  //TODO:Products data should come from server db
+      setProducts(Prod_Reel_Data);
+    };
+    getDevData();
+  }, [products]);
+
+  return (
     <div className={styles.HomePage}>
       <Banner />
-      <PromoReel title='Electronics' />
-      <PromoReel title='Computers' />
-      <PromoReel title='Mobile Phones' />
+      <PromoReel title='Electronics' productsData={products} />
+      <PromoReel title='Computers' productsData={products} />
+      <PromoReel title='Mobile Phones' productsData={products} />
     </div>
   )
 }
--- components/ui/PromoReel.js
+++ components/ui/PromoReel.js
@@ -1,27 +1,15 @@
-import { useEffect, useState } from 'react';
 import styles from '../../styles/PromoReel.module.css';
 import ProductCard from './ProductCard';
 
 const PromoReel = ({ title = 'Promo Products', productsData = [] }) => {
-  const [products, setProducts] = useState(productsData);
   
-  //TODO: comment out useEffect for Production Release
-  //-> Use development data for testing before server data is created
-  useEffect(() => {
-    async function getDevData() {
-      const {Prod_Reel_Data} = await import('../../dev/data');
-      setProducts(Prod_Reel_Data);
-    };
-    if(!productsData || productsData.length < 1 ) getDevData();
-  }, [products]);
-
   return (
     <div className={styles.ReelWrapper}>
       <div className={styles.ReelTitle}>
         {title}
       </div>
       <div className={styles.Reel}>
-        {products.map((p) => <ProductCard 
+        {productsData.map((p) => <ProductCard 
           key={p.index} 
           pixUrl={p.url} 
           name={p.name} 
