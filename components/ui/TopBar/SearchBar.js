f890982d43d921223683a5b154a994e1486bbed0
Burabari <burabaridev@gmail.com>
2021-11-25 13:04:22 +0100

    refactor SearchBar to a separate component

---
+++ components/ui/TopBar/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import styles from '../../../styles/SearchBar.module.css';
+
+const SearchBar = () => {
+  const [searchText, setSearchText] = useState('');
+  const placeHolderTxt = 'What are you looking for?';
+
+  const doSearch = (keyword) => {
+    // setAccountText(keyword || 'Sign In')
+  };
+
+  return (
+    <div className={styles.Wrapper}>
+      <div className='p-inputgroup'>
+        <InputText
+          placeholder={placeHolderTxt}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)} />
+        <Button icon='pi pi-search' onClick={(e) => doSearch(searchText)} />
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar;
